399ed9b028290608d7a807188a7e3f8e0666bd71
Xedza <277286108+Xedza@users.noreply.github.com>
2026-04-29 13:03:28 -0700

    Added Main

---
+++ main.cpp
@@ -0,0 +1,38 @@
+// < Bank System > //
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
+
+using namespace std;
+
+// < User Account System > //
+class user {
+    public:
+        int balance;
+        int creditscore;
+        string password;
+        string username;
+};
+
+// < Main INT > //
+
+int main() {
+    cout << "Hello, user.\n"
+    cout << "Welcome to the international < HAJEAIN BANKING SERVICES >\n"
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    cout << "Before you start, please enter in a username and password!\n";
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    cout << "\n";
+    cout << "Username: ";
+    cin << username;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    system("clear");
+    system("cls");
+    cout >> 
+}
+
+// < Wait function > //
+// std::this_thread::sleep_for(std::chrono::seconds(2)); //
